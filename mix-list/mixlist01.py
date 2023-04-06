d200a2e234a0677160d19b604eb39ccc98659262
darrinbliler <darrin.bliler@outlook.com>
2023-04-06 19:47:17 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#create a list
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#show the first item in the list
+print("The first item in the list (IP): " + my_list[0] )
+
+#show the second item in the list
+print("Port: " + str(my_list[1]))
+
+#show the third item in the list
+print("State: " + my_list[2])
---
+++ mix-list/mixlist02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#create a list of ips
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+#display only the IP addresses
+print("IP addresses in the list: " + iplist[3] + ", " + iplist[4])
