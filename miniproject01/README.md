0c536faede5932099f4d660f13a4bc173127c8d3
darrinbliler <darrin.bliler@outlook.com>
2023-04-12 20:06:54 +0000

    Version 3 mini project 1 - added nested if loop

--- miniproject01/README.md
+++ miniproject01/README.md
@@ -1,16 +1,4 @@
 Objective: 
 	• Allow the user to select from a list of 3 states and generate a road trip itinerary from the selected state.
 
-	1) Region selection
-		○ Provide a list of the regions in the input prompt
-		○ Include the states included in that region in the input prompt
-		a) Request user provide input
-			i. Errors
-				1) Input did not match a region
-				2) User did not provide an input
-				3) Input is not of data type string
-	2) City selection
-		○ First, list all the cities in the region with a description of each city. 
-		a) Request user to select three of those cities to build an itinerary. 
-
-Finally, export that itinerary to the user in an easy-to-read format. 
+	 
--- miniproject01/miniproject01.py
+++ miniproject01/miniproject01.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""This program will allow a user to select from a list of 3 states and generate a road trip itinerary from the selected state"""
+"""This program will allow a user to select from a list of 3 states and generate at least two road trip itineraries from the selected state"""
 
 def main():
-
-    while True:
+    cont_loop = True
+    while cont_loop == True:
         states = {
             "CA": "California",
             "NV": "Nevada",
@@ -11,8 +11,14 @@ def main():
         }
         states_long = states.values()
         print("The states to choose from include", states["CA"], ",", states["NV"], ", and", states["OR"])
+        
+        # have the user input a state they want to travel to
         sel_state = input("Please input which state you would like to travel to: ") 
+                
+        # capitalize state name
         sel_state = sel_state.title()
+
+        # loop through user's selected state and output itinerary
         if sel_state == 'California':
             calif_itin = {
                 "Itinerary 1" : "Los Angeles - Monterey - San Francisco",
@@ -20,7 +26,13 @@ def main():
             }
             for k, v in calif_itin.items():
                 print(k, v)
-            break
+            print("Would you like to select a new state?")
+            cont_loop = input("Y/N: ")
+            cont_loop = cont_loop.lower()
+            if cont_loop == "y":
+                cont_loop = True
+            else:
+                break
         elif sel_state == 'Nevada':
             nev_itin = {
                 "Itinerary 1" : "Las Vegas - Reno - Lake Tahoe",
@@ -28,7 +40,13 @@ def main():
             }
             for k, v in nev_itin.items():
                 print(k, v)
-            break
+            print("Would you like to select a new state?")
+            cont_loop = input("Y/N: ")
+            cont_loop = cont_loop.lower()
+            if cont_loop == "y":
+                cont_loop = True
+            else:
+                break
         elif sel_state == 'Oregon':
             or_itin = {
                 "Itinerary 1" : "Baker City - Portland - Medford",
@@ -36,9 +54,21 @@ def main():
             }
             for k,v in or_itin.items():
                 print(k, v)
-            break
+            print("Would you like to select a new state?")
+            cont_loop = input("Y/N: ")
+            cont_loop = cont_loop.lower()
+            if cont_loop == "y":
+                cont_loop = True
+            else:
+                break
         else:
-            print("Your input didn't match any of the above states, please input one of the three states.")
+            print("Your input didn't match any of the above states, would you like to continue?")
+            cont_loop = input("Y/N: ")
+            cont_loop = cont_loop.lower()
+            if cont_loop == "y":
+                cont_loop = True
+            else:
+                break
  
 if __name__ == "__main__":
     main()
