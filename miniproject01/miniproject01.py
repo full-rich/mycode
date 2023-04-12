2e7c0e4c82f7efd9ed1a81609829adc1922d4a29
darrinbliler <darrin.bliler@outlook.com>
2023-04-12 19:44:39 +0000

    Version 2 of miniproject - added while loop

--- miniproject01/miniproject01.py
+++ miniproject01/miniproject01.py
@@ -2,41 +2,43 @@
 """This program will allow a user to select from a list of 3 states and generate a road trip itinerary from the selected state"""
 
 def main():
-    states = {
-        "CA": "California",
-        "NV": "Nevada",
-        "OR": "Oregon",
-    }
-    states_long = states.values()
-    print("The states to choose from include", states["CA"], ",", states["NV"], ", and", states["OR"])
-    sel_state = input("Please input which state you would like to travel to: ")
-    sel_state = sel_state.title()
 
-
-    if sel_state == 'California':
-        calif_itin = {
-            "Itinerary 1" : "Los Angeles - Monterey - San Francisco",
-            "Itinerary 2" : "San Diego - Mammoth - Lake Tahoe"
+    while True:
+        states = {
+            "CA": "California",
+            "NV": "Nevada",
+            "OR": "Oregon",
         }
-        for k, v in calif_itin.items():
-            print(k, v)
-    elif sel_state == 'Nevada':
-        nev_itin = {
-            "Itinerary 1" : "Las Vegas - Reno - Lake Tahoe",
-            "Itinerary 2" : "Winnemuca - Reno - Las Vegas"
-        }
-        for k, v in nev_itin.items():
-            print(k, v)
-    elif sel_state == 'Oregon':
-        or_itin = {
-            "Itinerary 1" : "Baker City - Portland - Medford",
-            "Itinerary 2" : "Burns - Klamath Falls - Medford"
-        }
-        for k,v in or_itin.items():
-            print(k, v)
-    else:
-        print("Your input didn't match any of the above states, please input one of the three states.")
-    
+        states_long = states.values()
+        print("The states to choose from include", states["CA"], ",", states["NV"], ", and", states["OR"])
+        sel_state = input("Please input which state you would like to travel to: ") 
+        sel_state = sel_state.title()
+        if sel_state == 'California':
+            calif_itin = {
+                "Itinerary 1" : "Los Angeles - Monterey - San Francisco",
+                "Itinerary 2" : "San Diego - Mammoth - Lake Tahoe"
+            }
+            for k, v in calif_itin.items():
+                print(k, v)
+            break
+        elif sel_state == 'Nevada':
+            nev_itin = {
+                "Itinerary 1" : "Las Vegas - Reno - Lake Tahoe",
+                "Itinerary 2" : "Winnemuca - Reno - Las Vegas"
+            }
+            for k, v in nev_itin.items():
+                print(k, v)
+            break
+        elif sel_state == 'Oregon':
+            or_itin = {
+                "Itinerary 1" : "Baker City - Portland - Medford",
+                "Itinerary 2" : "Burns - Klamath Falls - Medford"
+            }
+            for k,v in or_itin.items():
+                print(k, v)
+            break
+        else:
+            print("Your input didn't match any of the above states, please input one of the three states.")
  
 if __name__ == "__main__":
     main()
