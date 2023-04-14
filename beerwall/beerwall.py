32e09df6ba91ab14d63ac274237bab619d515233
darrinbliler <darrin.bliler@outlook.com>
2023-04-14 17:33:18 +0000

    fixed

--- beerwall/beerwall.py
+++ beerwall/beerwall.py
@@ -7,7 +7,7 @@ def main():
     bottles_rem = 99
     for bottles_rem in range(99):
         while bottles_rem != 0:
-            print(f"{bottles_rem} {beer_start}/n {bottles_rem} {beer_start} {bottles_rem} {beer_fin}\n")
+            print(f"{bottles_rem} {beer_start}\n {bottles_rem} {beer_start} {bottles_rem} {beer_fin}\n")
             bottles_rem = bottles_rem - 1
 
 if __name__ == "__main__":
