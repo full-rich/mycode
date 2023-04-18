a46fa56904ed4e3ed922a7f054093a17e0eb8b05
darrinbliler <darrin.bliler@outlook.com>
2023-04-18 23:09:40 +0000

    v2 - comments moved to end of lines

--- my_projects/final_project/weather_requester.py
+++ my_projects/final_project/weather_requester.py
@@ -10,52 +10,35 @@ from pprint import pprint
 
 def main():
 
-    # get user-requested city
-    city_input = input("Please input a city: ")
+    city_input = input("Please input a city: ")    # get user-requested city
 
-    # format to capitalize first letter of city - if multiple words, capitalizes first letter of each word in city name
-    city_input_title = city_input.title()
+    city_input_title = city_input.title()    # format to capitalize first letter of city - if multiple words, capitalizes first letter of each word in city name
     
-    # get user-requested state
-    state_input = input("Please input a state: ")
+    state_input = input("Please input a state: ")    # get user-requested state
 
-    # place state into upper case
-    state_input_upper = state_input.upper()
+    state_input_upper = state_input.upper()    # place state into upper case
 
-    # print out the inputted city, state
-    print(f"You input {city_input_title}, {state_input_upper}")
+    print(f"You input {city_input_title}, {state_input_upper}")    # print out the inputted city, state
 
-    # store the city state as a key, value pair in python dictionary format
-    city_state_dict = {'city' : city_input_title, 'state' : state_input_upper}
+    city_state_dict = {'city' : city_input_title, 'state' : state_input_upper}    # store the city state as a key, value pair in python dictionary format
+
+    location_request = Nominatim(user_agent="dcb-weatherrequester")    # comply with Nominatim Usage Policy by specifiying custom user_agent
+
+    location = location_request.geocode(city_state_dict, exactly_one=True) # request location data generation for city, state dictioary
     
-
-#def geolocator(city, state):
-    # comply with Nominatim Usage Policy by specifiying custom user_agent
-    location_request = Nominatim(user_agent="dcb-weatherrequester")
-
-    # request location data generation for city, state dictioary
-    location = location_request.geocode(city_state_dict, exactly_one=True)
+    location_lat = location.latitude # retrieve location latitude
     
-    # retrieve location latitude
-    location_lat = location.latitude
-    
-    # retrieve location longitude
-    location_lon = location.longitude
+    location_lon = location.longitude    # retrieve location longitude
 
-    # truncate to 4 digits to comply with NWS weather API {latitude},{longitude} limits
-    location_lat_trunc = round(location_lat, 4)
-    location_lon_trunc = round(location_lon, 4)
+    location_lat_trunc = round(location_lat, 4)    # truncate to 4 digits to comply with NWS weather API {latitude},{longitude} limits
+    location_lon_trunc = round(location_lon, 4)    # truncate to 4 digits to comply with NWS weather API {latitude},{longitude} limits
     
     URL = "https://api.weather.gov/points/"
-    nws_api = requests.get(f"{URL}{location_lat_trunc},{location_lon_trunc}").json() # request grid endpoint from "forecast" property
-    #pprint(nws_api)
-    nws_forecast_url_properties = nws_api.get('properties')
-    nws_forecast_url = nws_forecast_url_properties.get('forecast')
-    # print(nws_forecast_url)
+    nws_api = requests.get(f"{URL}{location_lat_trunc},{location_lon_trunc}").json()    # request grid endpoint from "forecast" property
+    # pprint(nws_api)
+    nws_forecast_url_properties = nws_api.get('properties')    # parse properties to get to forecast url
+    nws_forecast_url = nws_forecast_url_properties.get('forecast')    # extract forecast url
+    print(nws_forecast_url)
 
 if __name__ == '__main__':
     main()
-
-#if __name__ == '__weather_requester__':
-    #city_state()
-    #geolocator()
