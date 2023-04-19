b6669efeb0d737798749eddfeeb17b99177402a9
darrinbliler <darrin.bliler@outlook.com>
2023-04-19 22:18:52 +0000

    final proj - extracted weather forecast

--- my_projects/final_project/weather_requester.py
+++ my_projects/final_project/weather_requester.py
@@ -12,17 +12,18 @@ def main():
 
     city_input = input("Please input a city: ")    # get user-requested city
 
-    city_input_title = city_input.title()    # format to capitalize first letter of city - if multiple words, capitalizes first letter of each word in city name
+    city_input = city_input.title()    # format to capitalize first letter of city - if multiple words, capitalizes first letter of each word in city name
     
     state_input = input("Please input a state: ")    # get user-requested state
 
-    state_input_upper = state_input.upper()    # place state into upper case
+    state_input = state_input.upper()    # place state into upper case
 
-    print(f"You input {city_input_title}, {state_input_upper}")    # print out the inputted city, state
+    print(f"You input {city_input}, {state_input}")    # print out the inputted city, state
 
-    city_state_dict = {'city' : city_input_title, 'state' : state_input_upper}    # store the city state as a key, value pair in python dictionary format
+    city_state_dict = {'city' : city_input, 'state' : state_input}    # store the city state as a key, value pair in python dictionary format
 
-    location_request = Nominatim(user_agent="dcb-weatherrequester")    # comply with Nominatim Usage Policy by specifiying custom user_agent
+
+    location_request = Nominatim(user_agent="dcb-weatherrequester-1259")    # comply with Nominatim Usage Policy by specifiying custom user_agent
 
     location = location_request.geocode(city_state_dict, exactly_one=True) # request location data generation for city, state dictioary
     
@@ -35,10 +36,24 @@ def main():
     
     URL = "https://api.weather.gov/points/"
     nws_api = requests.get(f"{URL}{location_lat_trunc},{location_lon_trunc}").json()    # request grid endpoint from "forecast" property
+    
     # pprint(nws_api)
     nws_forecast_url_properties = nws_api.get('properties')    # parse properties to get to forecast url
     nws_forecast_url = nws_forecast_url_properties.get('forecast')    # extract forecast url
-    print(nws_forecast_url)
+    #print(nws_forecast_url)
+
+    # request result from forecast url
+    nws_return_forecast = requests.get(nws_forecast_url).json()
+    #pprint(nws_return_forecast)
+
+    # extract relevant forecast dict
+    nws_return_fc_output = nws_return_forecast['properties']['periods']
+    #pprint(nws_return_fc_output)
+
+    print(f"Weather forecast for {city_input}, {state_input}")
+    # iterate through each period
+    for period in range(len(nws_return_fc_output)):
+        print(f"{nws_return_fc_output[period]['name']}: {nws_return_fc_output[period]['detailedForecast']}")
 
 if __name__ == '__main__':
     main()
